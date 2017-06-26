b48a8bcecce81c50ca37963724b39b05d3ff9d32
Michael Francoeur <mjfrancoeur@gmail.com>
2017-06-26 08:44:11 -0400

    Text changes

--- play.js
+++ play.js
@@ -1,6 +1,5 @@
 const gameboard = require('./board.js');
 const prompt = require('prompt-sync')();
-const clear = require('clear');
 
 function play() {
   clear();
@@ -8,14 +7,14 @@ function play() {
   let gameWon = false;
 
   let board = gameboard;
-  console.log(board.display);
+  printStringToConsole(board.display);
 
+  // console.log('Welcome to tic-tac-toe. You\'re the \'X\' and the computer is the \'O\'. If you wish to quit while playing, you can enter an empty string as your guess.');
   let numTurns = 1;
   while (!gameWon && numTurns < 10) {
     if (numTurns % 2) {
       gameWon = humansTurn(board, numTurns++);
     } else {
-      console.log('Computer\'s turn.');
       gameWon = computersTurn(board, numTurns++);
     }
   }
@@ -29,16 +28,32 @@ function play() {
 function humansTurn(board, numTurns) {
   let guess = null;
 
-  do {
-    guess = prompt('It\'s your turn! Enter a number (0 - 8) to pick a place or nothing to quit: ');
-  } while (guess < 0 || guess > 8 || board.cells[guess]);
+  for (let i = 0; guess === null || guess < 0 || guess > 8 || board.cells[guess]; i++) {
+    if (i === 0) {
+      guess = prompt('It\'s your turn! Enter a number (0 - 8) to pick a place or an empty string to quit: ');
+    } else {
+
+      if (board.cells[guess]) {
+        guess = prompt('That space is already filled, try another: ');
+      } else if (guess < 0 || guess > 8) {
+        guess = prompt('That was an invalid guess, try a number between 0 and 8: ');
+      }
+    }
+
+    if (guess === '') {
+      console.log('You\'ve decided to forfeit.');
+      return true;
+    }
+
+  }
+
   board.updateBoard(guess, 'X');
 
   // Print board to console
   printStringToConsole(board.display);
 
   // Check to see if that was a winning move
-  if (numTurns > 5) {
+  if (numTurns > 4) {
     if (board.checkForWin('X')) {
       console.log('Congratulations! You won! But what is a win, really?');
       return true;
@@ -47,6 +62,7 @@ function humansTurn(board, numTurns) {
   }
 }
 
+// Simulates the computer's turn
 function computersTurn(board, numTurns) {
   let guess = null;
 
@@ -68,10 +84,17 @@ function computersTurn(board, numTurns) {
 
 }
 
+// Prints the board string to console
 function printStringToConsole(str) {
-  // console.clear();
   clear();
+  console.log('TIC-TAC-TOE\n');
   console.log(str);
+}
+
+// Clears the terminal console
+function clear() {
+  process.stdout.write('\033[2J');
+  process.stdout.write('\033[0f');
 }
 
 play();
